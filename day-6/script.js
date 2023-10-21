638690c69f84d8377d00e35e536adfa0d9bb825f
VedantCodes <vedantcodes30@gmail.com>
2023-10-21 23:00:11 +0530

    Day 6 and Day 7

---
+++ day-6/script.js
@@ -0,0 +1,11 @@
+// Create the HTML elements
+const h1 = document.createElement('h1');
+h1.textContent = 'Hello👋🏻, World!🌏';
+const p = document.createElement('p');
+p.textContent = 'This HTML content is generated using JavaScript DOM manipulation 🚀.';
+const title = document.createElement('title');
+title.textContent = 'Day 6';
+// Append elements to the document's body
+document.body.appendChild(h1);
+document.body.appendChild(p);
+document.body.appendChild(title);
---
+++ day-7/index.js
@@ -0,0 +1,3 @@
+let randomNum = Math.floor(Math.random() * 100) + 1;
+
+document.write("Random Number: " + randomNum)
