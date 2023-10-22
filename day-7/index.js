29b652b78e029989877d1fae1e33254e5120e35a
VedantCodes <vedantcodes30@gmail.com>
2023-10-22 15:17:05 +0530

    Random number generator

--- day-7/index.js
+++ day-7/index.js
@@ -1,3 +1,12 @@
-let randomNum = Math.floor(Math.random() * 100) + 1;
+// Create a div element with the id "rand"
+let randomNum = document.createElement('div');
+randomNum.id = 'rand';
 
-document.write("Random Number: " + randomNum)
+// Generate a random number
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// Set the inner text of the div to display the random number
+randomNum.innerText = "Random Number Generator:  " + randomNumber;
+
+// Append the div to the body of the document
+document.body.appendChild(randomNum);
