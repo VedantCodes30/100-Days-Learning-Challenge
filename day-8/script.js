fdbcb4a1b44172651b8f65362f687bf0bd29964f
VedantCodes <vedantcodes30@gmail.com>
2023-11-14 20:12:20 +0530

    Day 8 Temperature Conversion
    On the day 8 of 100 days learning challenge.
    
    I have created a temperature Converter (Inspired by Brocode)and here we have an index.html file with a form tag and it contains radio buttons for selecting either Celsius or Fahrenheit.
    
    Then we take those inputs from the HTML file and access them in the script.js and create a function which executes the logic part for converting the temperatures using If Else statements.
    
    Finally, styled it using CSS.

--- day-8/script.js
+++
@@ -1,8 +0,0 @@
-const title = document.createElement('title');
-title.textContent = 'Day 6';
-
-const heading = document.createElement('h1');
-heading.textContent = 'Hello, !';
-
-document.body.appendChild(title);
-document.body.appendChild(heading);
